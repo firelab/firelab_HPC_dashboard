91a97edc94fbdb285f376dc19bab58bebafad35a
gunjan dayani <41314977+dgh007786@users.noreply.github.com>
2024-11-19 11:58:08 -0700

    updated next.config.js and package.json to correct the frontend deployment pipeline on apache ninjastorm server

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    assetPrefix: process.env.NODE_ENV === "production" ? "/firelab_HPC_dashboard/" : "",
-    basePath: process.env.NODE_ENV === "production" ? "/firelab_HPC_dashboard" : "",
-  };
-  
-  module.exports = nextConfig;
-  
+  output: 'export',
+};
+
+module.exports = nextConfig;
